918709ed545fb9d7f7fe08a492fff62b1a58ff7b
Lucas Amoncio <lucasamoncio@gmail.com>
2020-11-28 20:57:18 +0800

    Created craps class

---
+++ craps.rb
@@ -0,0 +1,40 @@
+class Craps
+  require_relative "dice"
+
+  attr_reader :win_state
+
+  def start_game
+    roll = roll_2d6
+    if seven_eleven?(roll)
+      self.win_state = true
+    elsif craps?(roll)
+      self.win_state = false
+    else
+      point = roll
+      loop.do
+        roll = roll_2d6
+        if roll == point
+          self.win_state = true
+        elsif roll == 7
+          self.win_state = false
+        end
+        break unless win_state.nil?
+      end
+    end
+  end
+
+  private
+
+  def roll_2d6
+    dice = Dice.new(6)
+    dice.roll(2)
+  end
+
+  def seven_eleven?(roll)
+    roll = 7 || roll = 11
+  end
+
+  def craps?(roll)
+    roll = 2 || roll = 3 || roll = 12
+  end
+end
